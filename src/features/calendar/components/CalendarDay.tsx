21017253c1972c244eaf8ba7a8d088d2c83dab51
daunload <daun4799@gmail.com>
2025-11-26 09:56:17 +0900

    캘린더 UI 개선

--- src/features/calendar/components/CalendarDay.tsx
+++ src/features/calendar/components/CalendarDay.tsx
@@ -1,4 +1,5 @@
 import { Todo } from '@/features/todos/types'
+import { Check } from 'lucide-react'
 import { forwardRef } from 'react'
 
 const CalendarDay = forwardRef<
@@ -10,10 +11,12 @@ const CalendarDay = forwardRef<
 		onClick?: () => void
 	}
 >(({ day, isToday, tasks, onClick }, ref) => {
+	const completedTasks = tasks.filter((task) => task.completed).length
+	const totalTasks = tasks.length
+
 	return (
 		<div
 			ref={ref}
-			key={day}
 			onClick={onClick}
 			className={`h-24 border rounded-xl p-2 flex flex-col items-start justify-start transition-all duration-200 hover:bg-bg-subtle/30 cursor-pointer hover:scale-105 active:scale-95 ${
 				isToday
@@ -21,20 +24,24 @@ const CalendarDay = forwardRef<
 					: 'border-border-soft text-text-main bg-bg-surface'
 			}`}
 		>
-			<span
-				className={`text-sm font-medium w-6 h-6 flex items-center justify-center rounded-full ${
-					isToday ? 'bg-primary text-text-inverse' : ''
-				}`}
-			>
-				{day}
-			</span>
-			{tasks.length > 0 && (
-				<div className="flex flex-col w-full h-full items-center justify-center">
-					{tasks.length > 0 && (
-						<div className="text-md items-center justify-center flex text-text-main">
-							{tasks.length}
-						</div>
-					)}
+			<div className="flex items-start justify-between">
+				<span
+					className={`text-sm font-semibold w-6 h-6 flex items-center justify-center rounded-full ${
+						isToday
+							? 'bg-primary text-text-inverse'
+							: 'text-text-main'
+					}`}
+				>
+					{day}
+				</span>
+			</div>
+
+			{totalTasks > 0 && (
+				<div className="flex items-center mt-2">
+					<Check className="w-4 h-4 text-green-600" />
+					<p className="text-xs text-text-sub font-medium">
+						{completedTasks}/{totalTasks}
+					</p>
 				</div>
 			)}
 		</div>
